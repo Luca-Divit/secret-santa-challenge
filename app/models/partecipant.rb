1a70111c631a558955e1a19a7b629f6c9e2b0171
LucaDamiano DiVittorio <luca.div@hotmail.com>
2022-12-26 20:35:18 +0000

    created the model match added the foreign key and the connection with aliases for partecipants and validation for uniqueness on the match

--- app/models/partecipant.rb
+++ app/models/partecipant.rb
@@ -1,3 +1,5 @@
 class Partecipant < ApplicationRecord
   belongs_to :draw
+  has_one :match_as_giver, class_name: "Match", foreign_key: :giver
+  has_one :match_as_receiver, class_name: "Match", foreign_key: :receiver
 end
